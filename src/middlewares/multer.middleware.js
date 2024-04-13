31c5a1f2877235f9e9b9388e4efd0defb13f24ae
Sankalpgupta0 <125144958+Sankalpgupta0@users.noreply.github.com>
2024-04-14 05:19:46 +0530

    duplicates in multer handeled

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,15 +1,14 @@
-import multer from "multer";
+import multer from 'multer'
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+    // cb means callback
+    destination: function(req, file, cb){
+        cb(null, './public/temp') // folder where the files will be stored temporarily
     },
-    filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
+    filename: function(req, file, cb) {
+        //TODO:  make sure that there are no duplicates in filenames (if any user uploads 5 fils with same name than all previous files will be overwritten )
+        cb(null,`${Date.now()}_${file.originalname}`)  // The image name is used as the original name by user
     }
-  })
-  
-export const upload = multer({ 
-    storage, 
 })
+
+export const upload = multer({storage:storage});
