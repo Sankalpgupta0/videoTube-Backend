9b131e23fb0541a0a9fb8c53802445f5e5999901
Sankalp Gupta <guptasankalp2004@gmail.com>
2024-06-17 14:20:55 +0530

    websocket implement

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -4,6 +4,62 @@ import {User} from "../models/user.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { WebSocketServer } from 'ws'; 
+import url from 'url'
+import {redis} from '../redis/index.js'
+
+const wss = new WebSocketServer({ port: process.env.SOCKET_PORT });
+
+wss.on('connection', async(ws, req) => {
+    console.log("client connected")
+    // console.log(req.url)
+    const queryParams = url.parse(req.url,true).query;
+    const user_id = queryParams._id;
+    console.log(user_id);
+    const user = await User.findById(user_id);
+    if(!user){
+        throw new ApiError(401, 'Not authenticated');
+    }
+
+    ws.on('error', (err) => {
+        console.log(err);
+    })
+    ws.on("close", () => {
+        console.log("Connection lost");
+    });
+    ws.on('message', async(message) => {
+
+        const redisData = await redis.get(`rateLimiting:chat:${user_id}`)
+        // console.log(redisData);
+        if(redisData >=5){
+            // res.status(429).json({ statusCode: 429, message: "Too many requests, please try after sometime" })
+            ws.send(JSON.stringify({
+                owner: true,
+                content: "Too many requests, please try after sometime"
+            }));
+            return
+        }
+
+        const str = message.toString()
+        // console.log(str);
+        
+        const tweet = await Tweet.create({
+            owner: user_id, 
+            content: str
+        });
+
+        redis.incr(`rateLimiting:chat:${user_id}`, function (err, res) {
+            console.log(res);
+        });
+        redis.expire(`rateLimiting:chat:${user_id}`, 60)
+        wss.clients.forEach((client) => {
+            client.send(JSON.stringify({
+                owner: user_id,
+                content: str
+            }));
+        })
+    });
+})
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
